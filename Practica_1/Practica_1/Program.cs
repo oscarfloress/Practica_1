0a024bd54e562fea2be4c2776162abe48caa2b79
oscarfloress <oscar.flores@alumno.udg.mx>
2015-02-24 19:22:06 -0600

    Declaracion de varibles y menu

--- Practica_1/Practica_1/Program.cs
+++ Practica_1/Practica_1/Program.cs
@@ -6,6 +6,17 @@ namespace Practica_1
     {
         static void Main(string[] args)
         {
+            Console.Title = "Practica_1";
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            int primero, segundo;
+            float radio;
+            Console.WriteLine("1 suma\n2 resta\n3 multiplicacion\n4 division\n5 area triangulo\n6 area circulo\n7 area cuadrado\n");
+            int opcion = int.Parse(Console.ReadLine());
+
+            Console.Write("\nPress any key to continue . . . ");
+            Console.ReadKey(true);
         }
     }
 }
